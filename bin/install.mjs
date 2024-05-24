ae1434502f71c7110ed8fe52d615547dd1738007
juji <jujiyangasli@gmail.com>
2024-05-24 23:51:05 +0700

    ascii art

--- bin/install.mjs
+++ bin/install.mjs
@@ -2,17 +2,25 @@
 
 import { 
   intro, outro,
-  isCancel,
+  isCancel, select,
   text 
 } from '@clack/prompts';
 import pc from 'picocolors';
 
+console.log(`
+ _  _  ____  _  _  ____  ____  _____  ___ 
+( \\( )( ___)( \\/ )(_  _)(  _ \\(  _  )/ __)
+ )  (  )__)  )  (   )(   )(_) ))(_)(( (__ 
+(_)\\_)(____)(_/\\_) (__) (____/(_____)\\___)
+
+v 0.1.0
+`)
+
 intro(pc.bgCyan(pc.bold(` Create NextDoc `)));
 
 const dir = await text({
-  message: 'Where do you want to create?',
+  message: 'Which directory will you be working?',
   placeholder: 'my-next-docs',
-  initialValue: 'my-next-docs',
   validate(value) {
     if (value.length === 0) return `Value is required!`;
   },
@@ -22,6 +30,24 @@ if(isCancel(dir)){
   outro('Canceled.')
   process.exit(0)
 }
+
+const pkgm = await select({
+  message: 'Which pkg manager do you use?',
+  options: [
+    { value: 'npm', label: 'npm' },
+    { value: 'pnpm', label: 'pnpm' },
+    { value: 'bun', label: 'bun' },
+    { value: 'yarn', label: 'yarn' },
+    { value: '_', label: '[I\'ll do the install on my own]' },
+  ],
+});
+
+if(isCancel(pkgm)){
+  outro('Canceled.')
+  process.exit(0)
+}
+
+
 
 
 outro('Ok, created :)')
